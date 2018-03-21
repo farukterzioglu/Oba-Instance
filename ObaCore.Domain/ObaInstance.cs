fa3af90b6b322c36207afcf35461d2cb9a4eb254
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2018-03-21 11:53:50 +0300

    new member proposal progress, unit tests

---
+++ ObaCore.Domain/ObaInstance.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObaCore.Domain
+{
+    public class ObaInstance
+    {
+        public string ObaHash { get; set; }
+        public List<ObaLaw> ObaLaws;
+        public List<ObaLaw> ObaLawProposals;
+        public List<LawAcceptanceRatioProposal> LawAcceptanceRatioProposals;
+        public List<AcceptanceProposal> AcceptanceProposals;
+        public List<Member> Members;
+
+        public double LawAcceptanceRatio { get; private set; }
+        public double MemberAcceptanceRatio { get; private set; }
+
+        public ObaInstance()
+        {
+            ObaLaws = new List<ObaLaw>();
+
+            ObaLawProposals = new List<ObaLaw>();
+            LawAcceptanceRatioProposals = new List<LawAcceptanceRatioProposal>();
+
+            AcceptanceProposals = new List<AcceptanceProposal>();
+            Members = new List<Member>();
+
+            LawAcceptanceRatio = 1;
+            MemberAcceptanceRatio = 1;
+        }
+
+        public int MemberCount => Members.Count;
+
+        #region Law Proposal
+        public string ProposeLaw(ObaLaw obaLaw)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VoteForLawProposal(string obaLawHash)
+        {
+
+        }
+        #endregion
+
+        #region LawAcceptanceRatio Proposal
+        private void ChangeLawAcceptanceRatio(double newRatio)
+        {
+            this.LawAcceptanceRatio = newRatio;
+        }
+
+        public string ProposeLawAcceptanceRatio(double newRatio)
+        {
+            var existing = LawAcceptanceRatioProposals.FirstOrDefault(x => x.NewRatio == newRatio);
+            if (existing != null) return existing.Hash;
+
+            string newHash = Helper.GetHashString(newRatio.ToString(CultureInfo.InvariantCulture));
+
+            LawAcceptanceRatioProposals.Add(new LawAcceptanceRatioProposal() { Hash = newHash, NewRatio = newRatio });
+
+            return newHash;
+        }
+
+        public void VoteLawProposalAcceptanceRatio(string hash, string voterId)
+        {
+            //Check if there is an active law proposal
+            var proposal = LawAcceptanceRatioProposals.FirstOrDefault(x => x.Hash == hash);
+            if (proposal == null || !proposal.IsProposalActive) return;
+
+            //Can not vote two times
+            if (proposal.VoterIds.Any(x => x == voterId)) return;
+
+            proposal.VoterIds.Add(voterId);
+
+            //Law accepted
+            if (proposal.VoterIds.Count >= this.MemberCount * LawAcceptanceRatio)
+            {
+                proposal.IsProposalActive = false;
+                ChangeLawAcceptanceRatio(proposal.NewRatio);
+            }
+        }
+        #endregion
+
+        #region New Member Voting
+
+        public void ProposeForAccentance(string newMemberHash)
+        {
+            //Check if already proposed 
+            var existing = AcceptanceProposals.FirstOrDefault(x => x.Hash == newMemberHash);
+            if (existing != null) return;
+
+            AcceptanceProposals.Add(new AcceptanceProposal() { Hash = newMemberHash });
+        }
+
+        public void VoteForNewMember(string newMemberHash, string voterHash)
+        {
+            //Check member
+            if (Members.Count > 0)
+            {
+                //Only exsiting members can vote
+                if (!Members.Select(x => x.Hash).Contains(voterHash)) return;
+            }
+            else
+            {
+                //If there isn't any member, only self voting is accepted
+                if (newMemberHash != voterHash) return;
+            }
+
+
+            //Check if there is an active member proposal
+            var proposal = AcceptanceProposals.FirstOrDefault(x => x.Hash == newMemberHash);
+            if (proposal == null || !proposal.IsProposalActive) return;
+
+            //Can not vote two times
+            if (proposal.VoterIds.Any(x => x == voterHash)) return;
+
+            //Update voters 
+            proposal.VoterIds.Add(voterHash);
+
+            //Check if member accepted
+            if (proposal.VoterIds.Count >= this.MemberCount * MemberAcceptanceRatio)
+            {
+                proposal.IsProposalActive = false;
+                Members.Add(new Member() { Hash = newMemberHash });
+            }
+        }
+        #endregion
+    }
+
+}
---
+++ ObaCore.Tests/ObaCoreTests.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObaCore.Domain;
+
+namespace ObaCore.Tests
+{
+    [TestClass]
+    public class ObaCoreTests
+    {
+        private ObaInstance _oba;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            //8eb6bfcf8da5c86717c2a3b927c19792b3fc55c85191f298d9ec2e4bea8ce39e
+            //e6e10d36854ed0ade3c5677d2cde228be5205712e0ea3456474ca38512f1025c
+            //bf4f6997dbcb57c8492f2d4f7a39f09f231b4d61ae9372e1ac8dcbc00a763281
+
+            _oba = new ObaInstance();
+        }
+
+        [TestMethod]
+        public void AddtoAcceptanceProposals()
+        {
+            var memberHash = Helper.GetHashString("8eb6bfcf8da5c86717c2a3b927c19792b3fc55c85191f298d9ec2e4bea8ce39e");
+
+            _oba.ProposeForAccentance(memberHash);
+
+            var first = _oba.AcceptanceProposals.First( x=> x.Hash == memberHash);
+
+            Assert.IsTrue(first.IsProposalActive);
+        }
+
+        [TestMethod]
+        public void AddstoAcceptanceProposals()
+        {
+            var memberHash = Helper.GetHashString("8eb6bfcf8da5c86717c2a3b927c19792b3fc55c85191f298d9ec2e4bea8ce39e");
+
+            _oba.ProposeForAccentance(memberHash);
+            _oba.ProposeForAccentance(memberHash);
+
+            var first = _oba.AcceptanceProposals.First(x => x.Hash == memberHash);
+
+            Assert.IsTrue(first.IsProposalActive);
+        }
+
+        [TestMethod]
+        public void VoteForAcceptanceFirstPerson()
+        {
+            var memberHash = Helper.GetHashString("8eb6bfcf8da5c86717c2a3b927c19792b3fc55c85191f298d9ec2e4bea8ce39e");
+
+            _oba.ProposeForAccentance(memberHash);
+            _oba.VoteForNewMember(memberHash, memberHash);
+
+            Assert.IsTrue(_oba.Members.Count > 0);
+            Assert.IsFalse(_oba.AcceptanceProposals.Any( x=> x.IsProposalActive == true));
+        }
+
+        [TestMethod]
+        public void VoteForAcceptanceCantVoteSelfIfObaHasMembers()
+        {
+            //Add him/her self
+            var memberHash = Helper.GetHashString("8eb6bfcf8da5c86717c2a3b927c19792b3fc55c85191f298d9ec2e4bea8ce39e");
+            _oba.ProposeForAccentance(memberHash);
+            _oba.VoteForNewMember(memberHash, memberHash);
+
+            //Propose another person
+            var member2Hash = Helper.GetHashString("e6e10d36854ed0ade3c5677d2cde228be5205712e0ea3456474ca38512f1025c");
+            _oba.ProposeForAccentance(member2Hash);
+
+            //Vote for self
+            _oba.VoteForNewMember(member2Hash, member2Hash);
+
+            //Can not vote for self 
+            Assert.IsTrue(_oba.Members.Count == 1);
+            Assert.IsTrue(_oba.AcceptanceProposals.First(x => x.IsProposalActive == true) != null);
+        }
+
+        [TestMethod]
+        public void VoteForAcceptanceSecondPerson()
+        {
+            //Add him/her self
+            var memberHash = Helper.GetHashString("8eb6bfcf8da5c86717c2a3b927c19792b3fc55c85191f298d9ec2e4bea8ce39e");
+            _oba.ProposeForAccentance(memberHash);
+            _oba.VoteForNewMember(memberHash, memberHash);
+
+            //Propose another person
+            var member2Hash = Helper.GetHashString("e6e10d36854ed0ade3c5677d2cde228be5205712e0ea3456474ca38512f1025c");
+            _oba.ProposeForAccentance(member2Hash);
+
+            //Vote for new person 
+            _oba.VoteForNewMember(member2Hash, memberHash);
+
+            //Can not vote for self 
+            Assert.IsTrue(_oba.Members.Count == 2);
+            Assert.IsFalse(_oba.AcceptanceProposals.Any(x => x.IsProposalActive == true));
+        }
+
+        [TestMethod]
+        public void VoteForAcceptance_NonMemberCantVote()
+        {
+            //Add him/her self
+            var memberHash = Helper.GetHashString("8eb6bfcf8da5c86717c2a3b927c19792b3fc55c85191f298d9ec2e4bea8ce39e");
+            var member2Hash = Helper.GetHashString("e6e10d36854ed0ade3c5677d2cde228be5205712e0ea3456474ca38512f1025c");
+
+            _oba.ProposeForAccentance(memberHash);
+            _oba.VoteForNewMember(memberHash, member2Hash);
+            
+            Assert.IsTrue(_oba.Members.Count == 0);
+            Assert.IsTrue(_oba.AcceptanceProposals.Any(x => x.IsProposalActive == true));
+        }
+
+        [TestMethod]
+        public void VoteForAcceptance_NonMemberCantVote_WhenBothHasProposal()
+        {
+            //Add him/her self
+            var memberHash = Helper.GetHashString("8eb6bfcf8da5c86717c2a3b927c19792b3fc55c85191f298d9ec2e4bea8ce39e");
+            _oba.ProposeForAccentance(memberHash);
+
+            var member2Hash = Helper.GetHashString("e6e10d36854ed0ade3c5677d2cde228be5205712e0ea3456474ca38512f1025c");
+            _oba.ProposeForAccentance(member2Hash);
+
+            _oba.VoteForNewMember(memberHash, member2Hash);
+            _oba.VoteForNewMember(member2Hash, memberHash);
+
+            Assert.IsTrue(_oba.Members.Count == 0);
+            Assert.IsTrue(_oba.AcceptanceProposals.Any(x => x.IsProposalActive == true));
+        }
+
+        [TestMethod]
+        public void VoteForAcceptance_NonMemberCantVote_WhenObaHasMember()
+        {
+            //Add him/her self
+            var memberHash = Helper.GetHashString("8eb6bfcf8da5c86717c2a3b927c19792b3fc55c85191f298d9ec2e4bea8ce39e");
+            _oba.ProposeForAccentance(memberHash);
+            _oba.VoteForNewMember(memberHash, memberHash);
+
+            //Second person proposal
+            var member2Hash = Helper.GetHashString("e6e10d36854ed0ade3c5677d2cde228be5205712e0ea3456474ca38512f1025c");
+            _oba.ProposeForAccentance(member2Hash);
+
+            var member3Hash = Helper.GetHashString("bf4f6997dbcb57c8492f2d4f7a39f09f231b4d61ae9372e1ac8dcbc00a763281");
+
+            _oba.VoteForNewMember(member2Hash, member3Hash);
+
+            Assert.IsTrue(_oba.Members.Count == 1);
+            Assert.IsTrue(_oba.AcceptanceProposals.Any(x => x.IsProposalActive == true));
+        }
+        
+        [TestMethod]
+        public void VoteForAcceptance_WhenObaHasMultipleMembers_NotEnoughVote()
+        {
+            //Add him/her self
+            var memberHash = Helper.GetHashString("8eb6bfcf8da5c86717c2a3b927c19792b3fc55c85191f298d9ec2e4bea8ce39e");
+            _oba.ProposeForAccentance(memberHash);
+            _oba.VoteForNewMember(memberHash, memberHash);
+
+            //Propose another person
+            var member2Hash = Helper.GetHashString("e6e10d36854ed0ade3c5677d2cde228be5205712e0ea3456474ca38512f1025c");
+            _oba.ProposeForAccentance(member2Hash);
+
+            //Vote for new person 
+            _oba.VoteForNewMember(member2Hash, memberHash);
+
+            //3. Person proposal
+            var member3Hash = Helper.GetHashString("bf4f6997dbcb57c8492f2d4f7a39f09f231b4d61ae9372e1ac8dcbc00a763281");
+            _oba.ProposeForAccentance(member3Hash);
+
+            //One member votes
+            _oba.VoteForNewMember(member3Hash, memberHash);
+
+            Assert.IsTrue(_oba.Members.Count == 2);
+            Assert.IsTrue(_oba.AcceptanceProposals.Any(x => x.IsProposalActive == true));
+        }
+
+        [TestMethod]
+        public void VoteForAcceptance_WhenObaHasMultipleMembers_EnoughVote()
+        {
+            //Add him/her self
+            var memberHash = Helper.GetHashString("8eb6bfcf8da5c86717c2a3b927c19792b3fc55c85191f298d9ec2e4bea8ce39e");
+            _oba.ProposeForAccentance(memberHash);
+            _oba.VoteForNewMember(memberHash, memberHash);
+
+            //Propose another person
+            var member2Hash = Helper.GetHashString("e6e10d36854ed0ade3c5677d2cde228be5205712e0ea3456474ca38512f1025c");
+            _oba.ProposeForAccentance(member2Hash);
+
+            //Vote for new person 
+            _oba.VoteForNewMember(member2Hash, memberHash);
+
+            //3. Person proposal
+            var member3Hash = Helper.GetHashString("bf4f6997dbcb57c8492f2d4f7a39f09f231b4d61ae9372e1ac8dcbc00a763281");
+            _oba.ProposeForAccentance(member3Hash);
+
+            //One member votes
+            _oba.VoteForNewMember(member3Hash, memberHash);
+            _oba.VoteForNewMember(member3Hash, member2Hash);
+
+            Assert.IsTrue(_oba.Members.Count == 3);
+            Assert.IsFalse(_oba.AcceptanceProposals.Any( x=> x.IsProposalActive == true));
+        }
+
+    }
+}
